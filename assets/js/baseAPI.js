6ea7b5473bec1073c17d0ff504879b4287c7bcdc
xiagor <1102503815@qq.com>
2020-12-10 16:52:23 +0800

    完成主页功能的开发

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -1,3 +1,14 @@
 $.ajaxPrefilter((options) => {
+    if (options.url.indexOf('/my/') !== -1) {
+        options.headers = {
+            Authorization: localStorage.getItem('token') || '',
+        }
+    }
+    options.complete = (res) => {
+        if (res.responseJSON.status === 1) {
+            location.href = '/login.html';
+            localStorage.removeItem('token');
+        }
+    }
     options.url = 'http://ajax.frontend.itheima.net' + options.url;
 });
---
+++ assets/js/index.js
@@ -0,0 +1,46 @@
+getUserInfo();
+
+function getUserInfo() {
+    $.ajax({
+        type: 'GET',
+        url: '/my/userinfo',
+        success: (res) => {
+            if (res.status !== 0) {
+                return layui.layer.msg('获取用户信息失败！');
+            }
+            renderAvatar(res.data);
+        }
+    })
+}
+
+function renderAvatar(user) {
+    let name = user.nickname || user.username,
+        first = name[0].toUpperCase();
+
+    $('#welcome').html('欢迎&nbsp;&nbsp;' + name);
+    if (user.user_pic === null) {
+        $('.layui-nav-img').hide();
+        $('.text_avatar')
+            .html(first)
+            .show();
+    } else {
+        $('.text_avatar').hide();
+        $('.layui-nav-img')
+            .attr('src', user.user_pic)
+            .show();
+
+    }
+}
+
+$('#btnLogout').on('click', () => {
+    let layer = layui.layer;
+    layer.confirm('确认退出登录？', {
+        icon: 3,
+        title: '提示'
+    }, function (index) {
+        localStorage.removeItem('token');
+        location.href = '/login.html';
+
+        layer.close(index);
+    });
+})
